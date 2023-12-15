5813167e64894b11a854377624375b174fa3f5b7
Michael Magdy <michaelmagdymagdy@gmail.com>
2023-12-16 00:23:07 +0200

    function fibo coded using memoization

--- test.cpp
+++ test.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
 using namespace std;
 
-long long fibo(int n){
-    //memoization
-    return n;
+long long fibo(int n, long long arr[]){
+    if (arr[n]==-1)
+        if (n<=1)
+            arr[n]= n;
+        else 
+            arr[n]= fibo(n-1,arr) + fibo(n-2,arr);
+    
+    return arr[n];
 }
 
 int main(){
@@ -12,13 +17,13 @@ int main(){
     {
         int n; cin>>n;
 
-        int arr[n];
+        long long arr[n+1];
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n+1; i++)
             arr[i] = -1;
 
-        long long res = fibo(n);
-        cout<<res;
+        long long res = fibo(n , arr);
+        cout<<res<<'\n';
         
     }
     
