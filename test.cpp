f46a3a31214262b31c3b920405c116255a454724
Michael Magdy <michaelmagdymagdy@gmail.com>
2023-12-15 23:55:15 +0200

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t; cin>>t;
+    while (t--)
+    {
+        int n; cin>>n;
+
+        int arr[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = -1;
+
+        int res = 1;//fibo(n);
+        cout<<res;
+        
+    }
+    
+
+
+
+
+    return 0;
+}
